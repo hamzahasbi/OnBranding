49e5f56a637009c7ac3d8342d8c60b15c27bef6b
Hamza Hasbi <hamza.hasbi@gmail.com>
2021-01-09 21:23:01 +0100

    chore: Connected to db

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/database');
 
+connectDB();
 
 app.get('/', (req, res) => res.send('server runing'));
 
