c9626ffcd8e10ec02ea3ecf39b86513af268e286
Hamza Hasbi <hamza.hasbi@gmail.com>
2021-01-17 16:46:47 +0100

    chore: taking care of env variables

--- backend/src/routes/api/users.js
+++ backend/src/routes/api/users.js
@@ -16,10 +16,14 @@ router.post('/', registrationRules, validate, async (req, res) => {
 
     try {
         const payload = await create({name, email, password});
+
         if (!payload) {
             return res.status(400).json({errors: [{msg: 'This email is already attached to an account'}]} )
         }
-        jwt.sign(payload, config.get("jwtSecret"), {expiresIn: 36000},
+
+        const secret = process.env.JWT_SECRET || config.get('jwtSecret');
+
+        jwt.sign(payload, secret, {expiresIn: 36000},
         (err, token) => {
             if (err) res.status(500).send('An error occured');
             res.status(201).json({token});
