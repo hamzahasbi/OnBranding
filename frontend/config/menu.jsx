73635201f58a59a4273a5759797db748d35ac831
Hamza Hasbi <hamza.hasbi@gmail.com>
2021-03-17 22:16:18 +0100

    chore: menu items object

---
+++ frontend/config/menu.jsx
@@ -0,0 +1,49 @@
+export const menu = [
+    {
+        'label' : 'Home',
+        'link'  : '/',
+        'icon'  : '',
+        'id'    : '1'
+    },
+    {
+        'label' : 'Dashboard',
+        'link'  : '/dashboard',
+        'icon'  : '',
+        'id'    : '2'
+
+    },
+    {
+        'label' : 'About',
+        'link'  : '/about',
+        'icon'  : '',
+        'id'    : '3'
+
+    },
+    {
+        'label' : 'Login',
+        'link'  : '/login',
+        'icon'  : '',
+        'id'    : '4'
+    },
+];
+
+export const OauthMenu = [
+    {
+        'label' : 'Badge',
+        'link'  : '#',
+        'icon'  : '',
+        'id'    : '1',
+    },
+    {
+        'label' : 'Settings',
+        'link'  : '#',
+        'icon'  : '',
+        'id'    : '2'
+    },
+    {
+        'label' : 'Log Out',
+        'link'  : '#',
+        'icon'  : '',
+        'id'    : '3'
+    },
+]
