65c3e43fabe2c0a02bb876e2d643dfcab5d7ffcc
Hamza Hasbi <hamza.hasbi@gmail.com>
2021-03-20 23:03:56 +0100

    fix: removed unused variable

--- frontend/pages/login.jsx
+++ frontend/pages/login.jsx
@@ -1,35 +1,24 @@
-// import React from "react";
 
-import {
-    Flex,
-    Stack,
-    Image,
-} from '@chakra-ui/react';
+import { Flex, Stack, Image } from '@chakra-ui/react';
 import LoginForm from '../components/forms/Login';
 
 export default function Login() {
     return (
         <>
-            <SplitScreen />
+            <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+                <Flex p={8} flex={1} align={'center'} justify={'center'}>
+                    <LoginForm />
+                </Flex>
+                <Flex flex={1}>
+                    <Image
+                        alt={'Login Image'}
+                        objectFit={'cover'}
+                        src={
+                            'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+                        }
+                    />
+                </Flex>
+            </Stack>
         </>
-    );
-}
-
-function SplitScreen() {
-    return (
-        <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
-            <Flex p={8} flex={1} align={'center'} justify={'center'}>
-            <LoginForm/>
-            </Flex>
-            <Flex flex={1}>
-                <Image
-                    alt={'Login Image'}
-                    objectFit={'cover'}
-                    src={
-                        'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
-                    }
-                />
-            </Flex>
-        </Stack>
     );
 }
