7ebe4a84c44da24b5d3789923e3baa2462d1c0cd
Hamza Hasbi <hamza.hasbi@gmail.com>
2021-03-20 23:03:06 +0100

    feat: registration process

--- frontend/components/forms/Registration.jsx
+++ frontend/components/forms/Registration.jsx
@@ -1,255 +1,143 @@
-// import {
-//     Text,
-//     Container,
-//     Button,
-//     Flex,
-//     Heading,
-//     Input,
-//     Stack,
-//     Box,
-//     SimpleGrid,
-//     Avatar,
-//     AvatarGroup,
-//     useBreakpointValue,
-//     Icon,
-// } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { validationAccount } from '../../config/validators';
+import {
+    Button,
+    Input,
+    Stack,
+    Box,
+    FormControl,
+    FormErrorMessage,
+} from '@chakra-ui/react';
+import FileUpload from '../ui-utils/file-upload';
 
-// const avatars = [
-//     {
-//         name: 'Ryan Florence',
-//         url: 'https://bit.ly/ryan-florence',
-//     },
-//     {
-//         name: 'Segun Adebayo',
-//         url: 'https://bit.ly/sage-adebayo',
-//     },
-//     {
-//         name: 'Kent Dodds',
-//         url: 'https://bit.ly/kent-c-dodds',
-//     },
-//     {
-//         name: 'Prosper Otemuyiwa',
-//         url: 'https://bit.ly/prosper-baba',
-//     },
-//     {
-//         name: 'Christian Nwamba',
-//         url: 'https://bit.ly/code-beast',
-//     },
-// ];
+export default function RegistartionForm() {
+    const { handleSubmit, register, errors, formState, control } = useForm({
+        mode: 'onChange',
+        reValidateMode: 'onChange',
+        defaultValues: {},
+        resolver: yupResolver(validationAccount),
+        context: undefined,
+        criteriaMode: 'firstError',
+        shouldFocusError: true,
+        // shouldUnregister: true,
+    });
 
-// function JoinOurTeam() {
-//     return (
-//         <Box position={'relative'}>
-//             <Container
-//                 as={SimpleGrid}
-//                 maxW={'7xl'}
-//                 columns={{ base: 1, md: 2 }}
-//                 spacing={{ base: 10, lg: 32 }}
-//                 py={{ base: 10, sm: 20, lg: 32 }}
-//             >
-//                 <Stack spacing={{ base: 10, md: 20 }}>
-//                     <Heading
-//                         lineHeight={1.1}
-//                         fontSize={{
-//                             base: '3xl',
-//                             sm: '4xl',
-//                             md: '5xl',
-//                             lg: '6xl',
-//                         }}
-//                     >
-//                         Senior web designers{' '}
-//                         <Text
-//                             as={'span'}
-//                             bgGradient="linear(to-r, red.400,pink.400)"
-//                             bgClip="text"
-//                         >
-//                             &
-//                         </Text>{' '}
-//                         Full-Stack Developers
-//                     </Heading>
-//                     <Stack direction={'row'} spacing={4} align={'center'}>
-//                         <AvatarGroup>
-//                             {avatars.map((avatar) => (
-//                                 <Avatar
-//                                     key={avatar.name}
-//                                     name={avatar.name}
-//                                     src={avatar.url}
-//                                     size={useBreakpointValue({
-//                                         base: 'md',
-//                                         md: 'lg',
-//                                     })}
-//                                     position={'relative'}
-//                                     zIndex={2}
-//                                     _before={{
-//                                         content: '""',
-//                                         width: 'full',
-//                                         height: 'full',
-//                                         rounded: 'full',
-//                                         transform: 'scale(1.125)',
-//                                         bgGradient:
-//                                             'linear(to-bl, red.400,pink.400)',
-//                                         position: 'absolute',
-//                                         zIndex: -1,
-//                                         top: 0,
-//                                         left: 0,
-//                                     }}
-//                                 />
-//                             ))}
-//                         </AvatarGroup>
-//                         <Text
-//                             fontFamily={'heading'}
-//                             fontSize={{ base: '4xl', md: '6xl' }}
-//                         >
-//                             +
-//                         </Text>
-//                         <Flex
-//                             align={'center'}
-//                             justify={'center'}
-//                             fontFamily={'heading'}
-//                             fontSize={{ base: 'sm', md: 'lg' }}
-//                             bg={'gray.800'}
-//                             color={'white'}
-//                             rounded={'full'}
-//                             width={useBreakpointValue({
-//                                 base: '44px',
-//                                 md: '60px',
-//                             })}
-//                             height={useBreakpointValue({
-//                                 base: '44px',
-//                                 md: '60px',
-//                             })}
-//                             position={'relative'}
-//                             _before={{
-//                                 content: '""',
-//                                 width: 'full',
-//                                 height: 'full',
-//                                 rounded: 'full',
-//                                 transform: 'scale(1.125)',
-//                                 bgGradient:
-//                                     'linear(to-bl, orange.400,yellow.400)',
-//                                 position: 'absolute',
-//                                 zIndex: -1,
-//                                 top: 0,
-//                                 left: 0,
-//                             }}
-//                         >
-//                             YOU
-//                         </Flex>
-//                     </Stack>
-//                 </Stack>
-//                 <Stack
-//                     bg={'gray.50'}
-//                     rounded={'xl'}
-//                     p={{ base: 4, sm: 6, md: 8 }}
-//                     spacing={{ base: 8 }}
-//                     maxW={{ lg: 'lg' }}
-//                 >
-//                     <Stack spacing={4}>
-//                         <Heading
-//                             color={'gray.800'}
-//                             lineHeight={1.1}
-//                             fontSize={{ base: '2xl', sm: '3xl', md: '4xl' }}
-//                         >
-//                             Join our team
-//                             <Text
-//                                 as={'span'}
-//                                 bgGradient="linear(to-r, red.400,pink.400)"
-//                                 bgClip="text"
-//                             >
-//                                 !
-//                             </Text>
-//                         </Heading>
-//                         <Text
-//                             color={'gray.500'}
-//                             fontSize={{ base: 'sm', sm: 'md' }}
-//                         >
-//                             We’re looking for amazing engineers just like you!
-//                             Become a part of our rockstar engineering team and
-//                             skyrocket your career!
-//                         </Text>
-//                     </Stack>
-//                     <Box as={'form'} mt={10}>
-//                         <Stack spacing={4}>
-//                             <Input
-//                                 placeholder="Firstname"
-//                                 bg={'gray.100'}
-//                                 border={0}
-//                                 color={'gray.500'}
-//                                 _placeholder={{
-//                                     color: 'gray.500',
-//                                 }}
-//                             />
-//                             <Input
-//                                 placeholder="firstname@lastname.io"
-//                                 bg={'gray.100'}
-//                                 border={0}
-//                                 color={'gray.500'}
-//                                 _placeholder={{
-//                                     color: 'gray.500',
-//                                 }}
-//                             />
-//                             <Input
-//                                 placeholder="+1 (___) __-___-___"
-//                                 bg={'gray.100'}
-//                                 border={0}
-//                                 color={'gray.500'}
-//                                 _placeholder={{
-//                                     color: 'gray.500',
-//                                 }}
-//                             />
-//                             <Button
-//                                 fontFamily={'heading'}
-//                                 bg={'gray.200'}
-//                                 color={'gray.800'}
-//                             >
-//                                 Upload CV
-//                             </Button>
-//                         </Stack>
-//                         <Button
-//                             fontFamily={'heading'}
-//                             mt={8}
-//                             w={'full'}
-//                             bgGradient="linear(to-r, red.400,pink.400)"
-//                             color={'white'}
-//                             _hover={{
-//                                 bgGradient: 'linear(to-r, red.400,pink.400)',
-//                                 boxShadow: 'xl',
-//                             }}
-//                         >
-//                             Submit
-//                         </Button>
-//                     </Box>
-//                     form
-//                 </Stack>
-//             </Container>
-//             <Blur
-//                 position={'absolute'}
-//                 top={-10}
-//                 left={-10}
-//                 style={{ filter: 'blur(70px)' }}
-//             />
-//         </Box>
-//     );
-// }
+    const onSubmit = (data) => console.log(data, 'Valide');
+    const onError = (errors) => console.log(errors, 'error');
 
-// export const Blur = (props) => {
-//     return (
-//         <Icon
-//             width={useBreakpointValue({ base: '100%', md: '40vw', lg: '30vw' })}
-//             zIndex={useBreakpointValue({ base: -1, md: -1, lg: 0 })}
-//             height="560px"
-//             viewBox="0 0 528 560"
-//             fill="none"
-//             xmlns="http://www.w3.org/2000/svg"
-//             {...props}
-//         >
-//             <circle cx="71" cy="61" r="111" fill="#F56565" />
-//             <circle cx="244" cy="106" r="139" fill="#ED64A6" />
-//             <circle cy="291" r="139" fill="#ED64A6" />
-//             <circle cx="80.5" cy="189.5" r="101.5" fill="#ED8936" />
-//             <circle cx="196.5" cy="317.5" r="101.5" fill="#ECC94B" />
-//             <circle cx="70.5" cy="458.5" r="101.5" fill="#48BB78" />
-//             <circle cx="426.5" cy="-0.5" r="101.5" fill="#4299E1" />
-//         </Icon>
-//     );
-// };
+    return (
+        <Box mt={10}>
+            <form onSubmit={handleSubmit(onSubmit, onError)}>
+                <Stack spacing={4}>
+                    <FormControl id="name" isInvalid={errors.name}>
+                        <Input
+                            placeholder="Name"
+                            bg={'gray.100'}
+                            border={0}
+                            color={'gray.500'}
+                            _placeholder={{
+                                color: 'gray.500',
+                            }}
+                            type="text"
+                            name="name"
+                            ref={register}
+                        />
+                        <FormErrorMessage>
+                            {errors.name && errors.name.message}
+                        </FormErrorMessage>
+                    </FormControl>
+                    <FormControl id="email" isInvalid={errors.email}>
+                        <Input
+                            placeholder="Email"
+                            bg={'gray.100'}
+                            border={0}
+                            color={'gray.500'}
+                            _placeholder={{
+                                color: 'gray.500',
+                            }}
+                            type="email"
+                            name="email"
+                            ref={register}
+                        />
+                        <FormErrorMessage>
+                            {errors.email && errors.email.message}
+                        </FormErrorMessage>
+                    </FormControl>
+                    <FormControl id="password" isInvalid={errors.password}>
+                        <Input
+                            placeholder="Password"
+                            bg={'gray.100'}
+                            border={0}
+                            color={'gray.500'}
+                            _placeholder={{
+                                color: 'gray.500',
+                            }}
+                            type="password"
+                            name="password"
+                            ref={register}
+                            autoComplete="off"
+                        />
+                        <FormErrorMessage>
+                            {errors.password && errors.password.message}
+                        </FormErrorMessage>
+                    </FormControl>
+                    <FormControl id="confirm" isInvalid={errors.confim}>
+                        <Input
+                            placeholder="Confirm your password"
+                            bg={'gray.100'}
+                            border={0}
+                            color={'gray.500'}
+                            _placeholder={{
+                                color: 'gray.500',
+                            }}
+                            type="password"
+                            ref={register}
+                            name="confirm"
+                            autoComplete="off"
+                        />
+                        <FormErrorMessage>
+                            {errors.confirm && errors.confirm.message}
+                        </FormErrorMessage>
+                    </FormControl>
+                    <FileUpload
+                        placeholder="Avatr"
+                        bg={'gray.100'}
+                        border={0}
+                        color={'gray.500'}
+                        _placeholder={{
+                            color: 'gray.500',
+                        }}
+                        type="password"
+                        acceptedFileTypes="image/png, image/jpeg"
+                        name="avatar"
+                        control={control}
+                    />
+                    <Button
+                        fontFamily={'heading'}
+                        bg={'gray.200'}
+                        color={'gray.800'}
+                        name="avatar"
+                    >
+                        Avatar
+                    </Button>
+                    <Button
+                        fontFamily={'heading'}
+                        mt={8}
+                        w={'full'}
+                        bgGradient="linear(to-r, red.400,pink.400)"
+                        color={'white'}
+                        _hover={{
+                            bgGradient: 'linear(to-r, red.400,pink.400)',
+                            boxShadow: 'xl',
+                        }}
+                        type="submit"
+                        isLoading={formState.isLoading}
+                    >
+                        Submit
+                    </Button>
+                </Stack>
+            </form>
+        </Box>
+    );
+}
