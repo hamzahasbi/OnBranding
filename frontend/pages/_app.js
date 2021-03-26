cfd5379feaed94174f61f41c2e9ea32105dffbba
Hamza Hasbi <hamza.hasbi@gmail.com>
2021-03-26 17:57:34 +0100

    build: layout wp

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,20 +1,21 @@
 import '../styles/globals.css';
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import { theme, Fonts } from '../config/theme';
-import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
+import SmallWithLogoLeft from '../layout/screen/Footer';
 
-const Navigation = dynamic(
-  () => import('../layout/screen/Navigation'),
-  { ssr: false }
-)
+const Navigation = dynamic(() => import('../layout/screen/Navigation'), {
+    ssr: false,
+});
 
 function GlobalApp({ Component, pageProps }) {
     return (
         <ChakraProvider theme={theme}>
-            <CSSReset/>
+            <CSSReset />
             <Fonts />
-            <Navigation/>
+            <Navigation />
             <Component {...pageProps} />
+            <SmallWithLogoLeft />
         </ChakraProvider>
     );
 }
